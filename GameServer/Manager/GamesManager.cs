b4bdc4469e41954b0be9b09a15a2c7732843dbce
CamilleRiolfi <84343113+CamilleRiolfi@users.noreply.github.com>
2022-10-26 10:52:34 +0200

    fix: GamesManager Init variables

--- GameServer/Manager/GamesManager.cs
+++ GameServer/Manager/GamesManager.cs
@@ -7,12 +7,11 @@ namespace GameServer.Manager
 {
     class GamesManager
     {
-        internal static List<Game> createdGames;
-        internal static List<string> startedGames;
+        internal static List<Game> createdGames = new List<Game>();
+        internal static List<string> startedGames = new List<string>();
         internal static void CreateNewGame(Player player1, Player player2)
         {
             createdGames.Add(new Game(player1, player2));
-
         }
         internal static void StartGames(Client client)
         {
