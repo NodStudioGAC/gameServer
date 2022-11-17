09b46bcda2bd486f459669d74dc8d5cfab8efa68
CamilleRiolfi <84343113+CamilleRiolfi@users.noreply.github.com>
2022-11-17 17:13:37 +0100

    up

--- GameServer/ServerUtils/DataReceiverUtils/Disconnection.cs
+++ GameServer/ServerUtils/DataReceiverUtils/Disconnection.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GameServer.ServerUtils.DataSenderUtils;
+using GameServer.ServerUtils.Models;
+using System;
 
 namespace GameServer.ServerUtils.DataReceiverUtils
 {
@@ -8,6 +10,8 @@ namespace GameServer.ServerUtils.DataReceiverUtils
         internal static void EndGame(Client client)
         {
             Console.WriteLine("start endgame");
+            Game currentGame = MorpionManager.GetGame(client);
+            MorpionSender.SendEndGame(Cell.STATE.VOID, currentGame);
             MorpionManager.EraseGame(MorpionManager.GetGame(client));
             client.Disconnect();
         }
